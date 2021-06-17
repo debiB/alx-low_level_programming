656ead7886822e200b0226381de2d983fdcb826f
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-17 04:44:47 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 1 (Success)
  */
-int main(void)
-{
-	char b='and that piece of art is useful\" - Dora Korpar, 2015-10-19';
-putchar(b);
-return (1);
-}
+int write(int filedes, const char *buf, unsigned int nbyte); 
+int main( int argc, char** argv ) 
+{ 
+ write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13); 
+ 
+   return (1); 
+} 
