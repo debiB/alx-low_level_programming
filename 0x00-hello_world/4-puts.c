0127c71d134aa5ad7f9b07667560c02e5f3401e7
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-17 03:34:38 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * program execution starts in main 
  *
- * Return 0
+ * Return type should be int
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
