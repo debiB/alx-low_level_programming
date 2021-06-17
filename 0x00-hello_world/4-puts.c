1a56a321cdd5e795894dbf69da2d30ce48ae6a88
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-17 03:36:59 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * program execution starts in main 
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
