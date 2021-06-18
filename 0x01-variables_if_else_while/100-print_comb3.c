5b78e49202453a23435f66603ec5b0b273454135
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-18 09:46:17 -0700

    variables,if else,while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,22 @@
 */
 int main(void)
 {
-  int tens;
-  int ones;
+	int tens;
+	int ones;
 
-  for (tens = 0; tens <= 8; tens++)
-    {
-      for (ones = tens + 1; ones <= 9; ones++)
-	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
-
-	  if (tens < 8)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-
-  return (0);
+	for (tens = 0; tens <= 8; tens++)
+		{
+		for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(tens + '0');
+				putchar(ones + '0');
+					if (tens < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	putchar('\n');
+	return (0);
 }
