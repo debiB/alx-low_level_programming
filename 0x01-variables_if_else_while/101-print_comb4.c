a6c5868cdb4930349a2bad8056126b10ed9a035c
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-18 10:39:06 -0700

    variables,if else,while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int thousands, tens, ones;
+
+	for (thousands = 0; thousands <= 9; thousands++)
+	{
+		for (tens = thousands + 1; tens <= 9; tens++)
+		{
+			 for (ones = tens + 1; ones <= 9; ones++)
+			 {
+			 putchar(thousands + '0');
+			 putchar(tens + '0');
+			 putchar(ones + '0');
+
+			if (thousands < 7)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
