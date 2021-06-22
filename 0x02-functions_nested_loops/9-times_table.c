ce06f89b5976c9cea48c03a07590ccd2d1c8980d
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-22 08:29:06 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * times_table - Print 9 times table
+ *
+ * Return: Void.
+ */
+
+void times_table(void)
+{
+	int x, y, multiplied_value;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			multiplied_value = x * y;
+			_putchar(multiplied_value + '0');
+			 _putchar(',');
+			  _putchar(' ');
+		}
+	
+		_putchar('\n');
+	}
+}
