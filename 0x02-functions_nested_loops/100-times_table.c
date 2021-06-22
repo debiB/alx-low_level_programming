85cb2c0d127ab7125e7a333fb7c48f48805fc656
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-22 09:41:31 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,47 @@
 #include "holberton.h"
-
 /**
-* print_times_table - Prints the n times table
-*
-* @n: number times table (0 < n <= 15)
-*
-* Return: no return
-*/
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
+ */
 void print_times_table(int n)
 {
-	int i, j, multiplied_value;
-	if(n>=0 && n<= 15)
+	int i, j, res;
+
+	if (!(n > 15 || n < 0))
 	{
-		for(i = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				multiplied_value = i*j;
-				if (j == 0)
+				res = (i * j);
+				if (j != 0)
 				{
-				_putchar(multiplied_value + '0');
+					_putchar(',');
+					_putchar(' ');
 				}
-				else if (multiplied_value >= 10)
+				if (res < 10 && j != 0)
 				{
-				_putchar(' ');
-				_putchar(multiplied_value / 10 + '0');
-				_putchar(multiplied_value % 10 + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
 				}
 				else
-				{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(multiplied_value + '0');
-				}
-				if (multiplied_value != (n*n))
-				{
-				_putchar(',');
-				}
+					_putchar((res % 10) + '0');
 			}
-
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
