c6b6de11513473e1bcaec1cf3150347c56f622a3
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-22 06:05:23 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include "string.h"
 /**
- * print_alphabet - print entire alphabet
+ * print_alphabet_x10 - print entire alphabet on 10 lines
  *
  * Return: Void.
  */
@@ -11,11 +11,11 @@ void print_alphabet_x10(void)
 	int i;
 	char charachter;
 
-	for(i=0; i< 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-	for(charachter='a'; charachter <= 'z'; charachter++)
+	for (charachter = 'a'; charachter <= 'z'; charachter++)
 	{
-			_putchar(charachter);
+		_putchar(charachter);
 	}		
 	_putchar('\n');
 	}
