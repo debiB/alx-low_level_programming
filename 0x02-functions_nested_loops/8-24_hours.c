c86efbdc982933974239ded90d7356cbb2737f1e
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-22 07:31:37 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,17 +9,17 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j <= 3; j++)
 		{
-			for (k = 0; k < 6; k++)
+			for (k = 0; k <= 5; k++)
 			{
-				for (l = 0; l < 10; l++)
+				for (l = 0; l <= 9; l++)
 				{
 					_putchar(i + '0');
 					_putchar(j + '0');
-				 	_putchar(':');
+					_putchar(':');
 					_putchar(k + '0');
 					_putchar(l + '0');
 					_putchar('\n');
