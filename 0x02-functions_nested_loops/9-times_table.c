3140f5775bc3d260c46a932037f08e3d4483578e
Dibora Berhanu <deborahberhanu644@gmail.com>
2021-06-22 08:36:03 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@
 void times_table(void)
 {
 	int x, y, multiplied_value;
+	multiplied_value = x * y;
 
 	for (y = 0; y < 10; y++)
 	{
 		for (x = 0; x < 10; x++)
 		{
-			multiplied_value = x * y;
 			_putchar(multiplied_value + '0');
 			 _putchar(',');
 			  _putchar(' ');
